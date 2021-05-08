0e3f7e866821eb9238d15317cfa2d91a6ecc3a93
AndresAyala01 <43978203+AndresRakde01@users.noreply.github.com>
2021-05-08 15:08:18 -0400

    ya no se muestra el scroll horizontal
    solo era agregar una letra xd

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -102,7 +102,7 @@ ul, ol {
         <br>
         <div class="container w-50 center "><h1>Recetas Saludables Para Niños</h1></div>
 
-        <div class="container-md container-inline w-75  " id="layer1" style="height:450px; overflow: scroll;"><br>
+        <div class="container-md container-inline w-75  " id="layer1" style="height:450px; overflow-y: scroll;"><br>
         <div class="row row-cols-1 row-cols-md-4 g-4">
         @foreach($recetas as $receta)
           <div class="col">
