4c298e76d388d63eb8d816602a66f9682498bdb3
estebanluis <luisestebanescobarrocha@gmail.com>
2021-05-16 18:26:05 -0400

    tamaño de subida de la imagen cambiado

--- app/Http/Controllers/recetaController.php
+++ app/Http/Controllers/recetaController.php
@@ -16,7 +16,7 @@ class recetaController extends Controller
     {
         $this->validate(request(),[
             'Nombre' => ['required' , 'max:40' , ],
-            'imagen' => ['required', 'size:5  kilobytes'  ],
+            'imagen' => ['required', 'size: 100 '  ],
             'Ingredientes' =>['required', 'max:1000'  ],
             'Edad' =>['required', 'max:5' ],
             'IngredientesAlternativos' =>['max:1000'  ],
