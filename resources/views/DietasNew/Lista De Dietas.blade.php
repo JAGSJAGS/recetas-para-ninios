7bf6727f4cb2e702c35e04867b44952cf163d005
AndresAyala01 <43978203+AndresRakde01@users.noreply.github.com>
2021-06-22 18:27:23 -0400

    css-andres-22-06
    CSS
    °index
         └---barra filtros
    °evaluacion alimenticia
    °dietas

--- resources/views/DietasNew/Lista De Dietas.blade.php
+++ resources/views/DietasNew/Lista De Dietas.blade.php
@@ -1,45 +1,48 @@
 @extends('extenciones.usuarioGral')
 @section('content')
 
-<h5 class="card-title">(Lista) Sugerencia De Dietas</h5>
+<div class="container w-50 center" style="font-family:Brush Script MT, arial, helvetica;"><h1 class="card-title">(Lista) Sugerencia De Dietas</h1></div>
+
+<div class="container container=inline w-100" id="listadietas">
 
 <div class="card-group">
 
-  <div class="card">
+  <div class="card" style="align-items: center;">
     <div class="card-body">
       <h5 class="card-title">1 Año De Edad</h5>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta</button>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta2</button>
     </div>
   </div>
 
 
-  <div class="card">
+  <div class="card" style="align-items: center;">
     <div class="card-body">
       <h5 class="card-title">2 Años De Edad</h5>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta</button>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta2</button>
     </div>
   </div>
 
 
-  <div class="card">
+  <div class="card" style="align-items: center;">
     <div class="card-body">
       <h5 class="card-title">3 Años De Edad</h5>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta</button>
     </div>
-    <div class="card-footer">
+    <div class="card-footer" id="dietass">
       <button type="button" class="btn btn-outline-dark">NombreDeLaDieta2</button>
     </div>
+  </div>
   </div>
 @endsection
